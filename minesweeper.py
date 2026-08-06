b79601375c52e74f2ca13ab334dbdc1ec5a13fc6
DanBianchini <danbianchini64@gmail.com>
2026-08-05 22:47:12 -0400

    Refactored into classes inheriting from Tkinter classes to allow for better custom interfacing between widgets.

--- minesweeper.py
+++ minesweeper.py
@@ -34,6 +34,7 @@ class Tile(Button):
         # iterate thru 3x3 square with (x, y) at the center
         for j in (self.y - 1, self.y, self.y + 1):
             for i in (self.x - 1, self.x, self.x + 1):
+
                 # if this is the middle tile (the one we're generating this for), skip
                 if j == self.y and i == self.x:
                     continue
@@ -106,41 +107,36 @@ class Tile(Button):
         # configure button
         self.config(text=Tile.FLAG if self.cget('text') == '' else '')
 
-class Minesweeper:
-    def __init__(self, width: int, height: int, num_mines: int):
+class Minefield(ttk.Frame):
+    def __init__(self, board: Tk, width: int, height: int, num_mines: int):
         # initialize instance variables
         self.width = width
         self.height = height
         self.num_mines = num_mines
         self.flag_count = 0
 
-        # create board
-        self.board = Tk() # create board
-        self.board.title("Minesweeper") # set title bar text
-
-        # create minefield Frame
-        self.minefield = ttk.Frame(self.board, padding=0) # create frame in board
-        self.minefield.grid()
-
-        # generate the minefield in the 'minefield' Frame
-        safe_spot = (int(width/2), int(height/2))
-        Minesweeper.plant_mines(self.minefield, width, height, safe_spot, *Minesweeper.generate_minefield_random(self.minefield, width, height, num_mines, safe_spot))
+        super().__init__(board, padding=0) # call parent constructor function
+        self.grid() # set up grid
 
     def load(self):
+        # generate the minefield in the Frame
+        safe_spot = (int(self.width/2), int(self.height/2))
+        self.plant_mines(safe_spot, self.generate_minefield_random(safe_spot))
+
         # iterate over all tiles
-        for tile in self.minefield.grid_slaves():
+        for tile in self.grid_slaves():
             tile.generate_adjacent()
             tile.calculate_threat_level()
 
-    def generate_minefield_test(minefield: ttk.Frame, width: int, height: int, num_mines: int, safe_spot: tuple[int, int]) -> tuple[tuple[int, int]]:
+    def generate_minefield_test(self, safe_spot: tuple[int, int]) -> tuple[tuple[int, int]]:
         result = [] # will store coordinate pairs
 
         # generate simple coordinates
-        for y in range(height):
-            for x in range(width):
+        for y in range(self.height):
+            for x in range(self.width):
 
                 # check if done
-                if len(result) == num_mines:
+                if len(result) == self.num_mines:
                     return tuple(result)
 
                 # skip safe spot
@@ -150,14 +146,15 @@ class Minesweeper:
                 # plant mines starting from the upper left corner going to the right until mines run out
                 result.append((x, y))
 
-    def generate_minefield_random(minefield: ttk.Frame, width: int, height: int, num_mines: int, safe_spot: tuple[int, int]) -> tuple[tuple[int, int]]:
+    def generate_minefield_random(self, safe_spot: tuple[int, int]) -> tuple[tuple[int, int]]:
             result = [] # will store coordinate pairs
             mines_planted = 0 # will store the # of mines planted
             
             # generate coordinates
-            while mines_planted < num_mines:
+            while mines_planted < self.num_mines:
+
                 # generate random coordinates
-                coords = (randint(0, width - 1), randint(0, height - 1))
+                coords = (randint(0, self.width - 1), randint(0, self.height - 1))
 
                 # if this is the safe spot (or adjacent to it), try a different spot
                 if (coords[0] in (safe_spot[0] - 1, safe_spot[0], safe_spot[0] + 1)) and (coords[1] in (safe_spot[1] - 1, safe_spot[1], safe_spot[1] + 1)):
@@ -171,12 +168,13 @@ class Minesweeper:
             # return the coordinates
             return tuple(result)
 
-    def plant_mines(minefield: ttk.Frame, width: int, height: int, safe_spot: tuple[int, int], *mine_coords: tuple[int, int]):
+    def plant_mines(self, safe_spot: tuple[int, int], mine_coords: tuple[tuple[int, int]]):
         mines_planted = 0 # keep track of how many mines have been planted
         
         # create tile buttons in frame
-        for y in range(height):
-            for x in range(width):
+        for y in range(self.height):
+            for x in range(self.width):
+
                 # if these coordinates are one of the pairs listed, plant a mine
                 plant_mine = False
                 if (x, y) in mine_coords:
@@ -187,9 +185,15 @@ class Minesweeper:
                 is_safe_spot = ((x, y) == safe_spot)
 
                 # create the tile
-                Tile(x, y, minefield, plant_mine, is_safe_spot).grid(column=x, row=y)
+                Tile(x, y, self, plant_mine, is_safe_spot).grid(column=x, row=y)
+
+class Board(Tk):
+    def __init__(self, width: int, height: int, num_mines: int):
+        super().__init__() # call super constructor
+        self.title("Minesweeper") # set title bar text
+        self.minefield = Minefield(self, width, height, num_mines) # create Minefield Frame in Board
+        self.minefield.load() # call load function on Minefield instance after initialization (required)
 
 if __name__ == "__main__":
-    ms = Minesweeper(25, 25, 150)
-    ms.load()
-    ms.board.mainloop()
+    board = Board(25, 25, 100)
+    board.mainloop()
